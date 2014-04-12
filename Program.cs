03f3cba615f3de8f0f23811dd3d430d117e4e9f8
Lidia <lidia.s.bobeva@gmail.com>
2014-04-12 19:01:25 +0300

    Test

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _6NamesListInAlphabetOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("UnorderedListWithNames.txt");
+            using (reader)
+            {
+                string line = reader.ReadLine();
+                List<string> nameList = new List<string>();
+                while (line != null)
+                {
+                    nameList.Add(line);
+                    line = reader.ReadLine();
+                }
+                nameList.Sort();
+                StreamWriter writer = new StreamWriter("OrderedListWithNames.txt");
+                using (writer)
+                {
+                    foreach (string name in nameList)
+                    {
+                        writer.WriteLine(name);
+                    }
+                }
+            }
+        }
+    }
+}
